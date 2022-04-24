e1e4f2856f49d20a1723769d92cb348b428bc573
Silja <silja.vj@gmail.com>
2022-04-24 21:33:45 +0200

    Add project files.

---
+++ Hangman/Finder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class Finder
+    {
+        internal static List<char> Word()
+        {
+            List<string> wordList = new List<string>(File.ReadAllLines(@"EnglishDictionary.txt"));
+
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(0, wordList.Count);
+            string randomWord = wordList[randomNumber];
+
+            Console.WriteLine(randomWord);
+
+            List<char> charList = new List<char>();
+
+            charList.AddRange(randomWord);
+
+            return charList;
+        }
+
+        internal static List<char> Underscore(List<char> charList)
+        {
+            List<char> underscoreList = new List<char>();
+
+            for (int i = 0; i < charList.Count; i++)
+            {
+                underscoreList.Add('_');
+                Console.Write(underscoreList[i]);
+            }
+
+            return underscoreList;
+        }
+    }
+}
---
+++ Hangman/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Hangman
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello motherfucker\n");
+
+            List<char> charList = new List<char>(Finder.Word());
+            List<char> underscoreList = new List<char>(Finder.Underscore(charList));
+
+            Methods.NewLine();
+
+            do
+            {
+                char userGuess = Console.ReadKey().KeyChar;
+                //Thread.Sleep(100);
+
+                Methods.NewLine();
+
+                bool doesContain = charList.Contains(userGuess);
+
+                Regex regexCheck = new Regex(@"[a-z]");
+                Match isValid = regexCheck.Match(userGuess.ToString());
+
+                if (doesContain && isValid.Success)
+                {
+                    for (int i = 0; i < charList.Count; i++)
+                    {
+                        if (charList[i].Equals(userGuess))
+                        {
+                            underscoreList[i] = charList[i];
+                        }
+
+                        Console.Write(underscoreList[i]);
+                    }
+                }
+                if (!isValid.Success)
+                {
+                    Console.WriteLine("Enter valid character\n");
+                }
+
+            } while (underscoreList.Contains('_'));
+
+        }
+    }
+
+    public class Methods
+    {
+        public static void NewLine()
+        {
+            Console.WriteLine(Environment.NewLine);
+        }
+    }
+}
